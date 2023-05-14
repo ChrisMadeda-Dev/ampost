d987f5ac1783c5553177374eda03ffdd7adf07a6
chrismadeda-Dev <chrismadeda.dev@gmail.com>
2023-05-15 01:27:08 +0300

    first commit

---
+++ src/components/Firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCMUdQT4AHTy-COQKFC9a4ChlnWAUF1LgM",
+  authDomain: "ampost-d5d16.firebaseapp.com",
+  projectId: "ampost-d5d16",
+  storageBucket: "ampost-d5d16.appspot.com",
+  messagingSenderId: "105014549893",
+  appId: "1:105014549893:web:547952cfda9492bd8678ec",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
